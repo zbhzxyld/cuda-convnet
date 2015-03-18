2115ba998e4018523150bf361196c962e82f5de5
zbhzxyld <frank.xy.zhu@gmail.com>
2015-03-18 10:08:48 +0800

    65x65 data provider

--- convdata.py
+++ convdata.py
@@ -268,5 +268,47 @@ class XGW_ONLINE_55x55_4131_DataProvider(DataProvider):
     def get_num_classes(self):
         return 4131        
                 
+# XGW - xgw-25crop - part2 - 55x55x3-SN online
+class XGW_DEEPID_ONLINE_65X65_4131_DataProvider(DataProvider):
+    def __init__(self, data_dir, batch_range, init_epoch=1, init_batchnum=None, dp_params={}, test=False):
+        DataProvider.__init__(self, data_dir, batch_range, init_epoch, init_batchnum, dp_params, test)
+        root_batch = h5py.File('%s/root_batch_65x65.mat'%(self.data_dir), 'r')
+        self.num_colors = 3
+        self.img_size = 65
+
+
+        self.m = root_batch['m'][:,:].T
+        self.imgs = root_batch['imgs'][:,:].T
+
+
+
+    def get_next_batch(self):
+        #epoch, batchnum, datadic = DataProvider.get_next_batch(self)
+        epoch, batchnum = self.curr_epoch, self.curr_batchnum
+        self.advance_batch()
+
+        # random shuffle
+        datadic = scipy.io.loadmat('%s/data_batch_%d.mat' % (self.data_dir, batchnum))
+        index = n.concatenate(datadic['index'], axis=1)-1
+        d = {}
+        d['data'] = n.require(self.imgs[:,index] - self.m, dtype=n.single, requirements='C')
+        d['labels'] = n.require(datadic['labels'].reshape((1, d['data'].shape[1])), dtype=n.single, requirements='C')-1
+        return epoch, batchnum, [d['data'], d['labels']]
+
+
+    # Returns the dimensionality of the two data matrices returned by get_next_batch
+    # idx is the index of the matrix.
+    def get_data_dims(self, idx=0):
+        return self.img_size**2 * self.num_colors if idx < 1 else 1
+
+    # Takes as input an array returned by get_next_batch
+    # Returns a (numCases, imgSize, imgSize, 3) array which can be
+    # fed to pylab for plotting.
+    # This is used by shownet.py to plot test case predictions.
+    def get_plottable_data(self, data):
+        return n.require(data.T.reshape(data.shape[1], self.img_size, self.img_size), dtype=n.single)
+
+    def get_num_classes(self):
+        return 4131
 
                 
--- convnet.py
+++ convnet.py
@@ -197,7 +197,7 @@ class ConvNet(IGPUModel):
 
         DataProvider.register_data_provider('xgw-1-sn-4213', 'XGW-55x55x3-SN 4213', XGW1SN4213DataProvider)
         DataProvider.register_data_provider('xgw-online-55x55-4131', 'XGW Online 55x55x3 with 4131 people', XGW_ONLINE_55x55_4131_DataProvider)
-
+        DataProvider.register_data_provider('xgw-deepid-online-65x65-4131', 'XGW-65x65x3-Deepid Online 4213', XGW_DEEPID_ONLINE_65X65_4131_DataProvider)
         return op
     
 if __name__ == "__main__":
